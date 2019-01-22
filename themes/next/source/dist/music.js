1afee55f71531ef13dddf41a14e909c5dffc332b
ebonyzhang <15069013006@163.com>
2019-01-22 17:34:43 +0800

    添加音乐and提交新的日志

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "陪我长大",
+        artist: '段奥娟',
+        url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/9d8ec5079fdc1671b3ed.mp3',
+        cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+      },
+      {
+        name: '漫步在岁月洪流',
+        artist: 'KasaYAYA',
+        url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/5a02e61685c2b044dc26.mp3',
+        cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+      },
+      {
+        name: '别再闹了',
+        artist: '毛不易',
+        url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/c6148d63e426d91008ce.mp3',
+        cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+      },
+	  {
+        name: '蝶恋花',
+        artist: '项斯华',
+        url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/7d5d7861f3850be9d956.mp3',
+        cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+      },
+	  {
+		name: '沧海一声笑',
+        artist: '付娜',
+		url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/caa694b40da12dec59d7.mp3',
+		cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+	  },
+	  {
+		name: '知否知否',
+        artist: '郁可唯&胡夏',
+		url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/c9d941f887183a721b6e.mp3',
+		cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+	  },
+	  {
+		name: '不哭',
+        artist: 'sunnee',
+		url: 'http://lc-9LpAkkox.cn-n1.lcfile.com/b7adbfcfb83d05874fd5.mp3',
+		cover: 'http://lc-9LpAkkox.cn-n1.lcfile.com/2c76d5daabed3a5fddac.png',
+	  }
+    ]
+});
